39ac8f733d9d11569bbda9681eba2d2c696030cd
Tom O'Connell <toconnel@caltech.edu>
2022-06-03 01:14:51 -0700

    Add initial dev requirements in setup.py 'dev' extras_require

--- setup.py
+++ setup.py
@@ -9,6 +9,19 @@ setup(
         # https://stackoverflow.com/questions/32688688
         'hong2p @ git+https://github.com/ejhonglab/hong2p',
     ],
+    extras_require={
+        'dev': [
+            'pre-commit',
+            'pytest',
+            'ipdb',
+
+            'sphinx',
+            'sphinx-rtd-theme',
+            'sphinxcontrib-apidoc',
+            'sphinx-prompt',
+            'sphinx-autodoc-typehints',
+        ],
+    },
     author="Tom O'Connell",
     author_email='toconnel@caltech.edu',
     license='GPLv3',
