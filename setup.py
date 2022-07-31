4ac369ac95f796e095be811852fc31b489d2d3f1
Tom O'Connell <toconnel@caltech.edu>
2022-07-31 13:45:39 -0700

    Hack to allow packages specifying '-e ../natmix' in requirements.txt to work w/ pip while also getting hong2p from github if not already installed

--- setup.py
+++ setup.py
@@ -1,14 +1,28 @@
 
 from setuptools import setup, find_packages
 
+
+try:
+    import hong2p
+    # Just to support installing a local copy for development.
+    install_requires = []
+
+except ImportError:
+    install_requires = [
+        # TODO how to get this to not cause pip depedency resolver to fail when i want
+        # to use an editable local install? and ideally without breaking the ability to
+        # make wheels... (e.g. by adding a conditional checking if hong2p is installed,
+        # like i'm currently doing)
+        #
+        # https://stackoverflow.com/questions/32688688
+        'hong2p @ git+https://github.com/ejhonglab/hong2p',
+    ]
+
 setup(
     name='natmix',
     version='0.0.0',
     packages=find_packages(),
-    install_requires=[
-        # https://stackoverflow.com/questions/32688688
-        'hong2p @ git+https://github.com/ejhonglab/hong2p',
-    ],
+    install_requires=install_requires,
     extras_require={
         'dev': [
             'pre-commit',
